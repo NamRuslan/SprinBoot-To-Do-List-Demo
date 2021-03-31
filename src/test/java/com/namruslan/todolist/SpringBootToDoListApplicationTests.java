1dec510c26f5bd8f49e0b07bdda83f7bf9aa6fb7
NamRuslan <rus.nam.96@bk.ru>
2021-03-31 17:27:59 +0300

    Created project

---
+++ src/test/java/com/namruslan/todolist/SpringBootToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namruslan.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
