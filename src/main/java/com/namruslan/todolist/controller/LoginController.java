45e3e9b0d68c4868b11125aca00495621a139ae1
NamRuslan <rus.nam.96@bk.ru>
2021-04-01 15:24:46 +0300

    Added representation classes for user and todo Added feature: new user registration

--- src/main/java/com/namruslan/todolist/controller/LoginController.java
+++ src/main/java/com/namruslan/todolist/controller/LoginController.java
@@ -1,10 +1,20 @@
 package com.namruslan.todolist.controller;
 
+import com.namruslan.todolist.repr.UserRepr;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @GetMapping("/login")
     public String loginPage() {
@@ -12,8 +22,15 @@ public class LoginController {
     }
 
     @GetMapping("/register")
-    public String registerPage() {
+    public String registerPage(Model model) {
+        model.addAttribute("user", new UserRepr());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute("user") @Valid UserRepr userRepr) {
+        logger.info("New user {}", userRepr);
+        return "redirect:/login";
     }
 
 
