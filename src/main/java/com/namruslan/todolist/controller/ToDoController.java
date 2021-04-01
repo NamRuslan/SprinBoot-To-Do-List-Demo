d02b19e6101a61dd7dbfbd4e35353e837d7693e8
NamRuslan <rus.nam.96@bk.ru>
2021-04-01 13:04:01 +0300

    Added simple controllers (@GetMapping): - index; - todo; - login.

---
+++ src/main/java/com/namruslan/todolist/controller/ToDoController.java
@@ -0,0 +1,22 @@
+package com.namruslan.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToDoController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/todo")
+    public String todoPage() {
+        return "todo";
+    }
+
+}
+
+
+
