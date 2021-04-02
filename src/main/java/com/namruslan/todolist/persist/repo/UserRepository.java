ccdcbb86306b4e7d4caec9cd54a40f4f062875df
NamRuslan <rus.nam.96@bk.ru>
2021-04-02 15:56:03 +0300

    Added feature - enabled spring security: - Now only authorized user can use "todo" page; - Web application is secured from unauthorized user; - Password encoding; - Enabled login; - User authentication.

--- src/main/java/com/namruslan/todolist/persist/repo/UserRepository.java
+++ src/main/java/com/namruslan/todolist/persist/repo/UserRepository.java
@@ -4,6 +4,12 @@ import com.namruslan.todolist.persist.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    boolean existsByUsername(String username);
+
+    Optional<User> getUserByUsername(String username);
 }
---
+++ src/main/java/com/namruslan/todolist/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.namruslan.todolist.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+
+}
